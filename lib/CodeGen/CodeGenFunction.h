5340d522c3f1d72ac560a4cf6209f6fb44b8840b
Alexey Bataev <a.bataev@hotmail.com>
2017-09-22 12:58:34 -0400

    [OPENMP] Handle re-declaration of captured variables in CodeGen.
    If the captured variable has re-declaration we may end up with the
    situation where the captured variable is the re-declaration while the
    referenced variable is the canonical declaration (or vice versa). In
    this case we may generate wrong code. Patch fixes this situation.

--- lib/CodeGen/CodeGenFunction.h
+++ lib/CodeGen/CodeGenFunction.h
@@ -233,9 +233,9 @@ public:
         if (I->capturesThis())
           CXXThisFieldDecl = *Field;
         else if (I->capturesVariable())
-          CaptureFields[I->getCapturedVar()] = *Field;
+          CaptureFields[I->getCapturedVar()->getCanonicalDecl()] = *Field;
         else if (I->capturesVariableByCopy())
-          CaptureFields[I->getCapturedVar()] = *Field;
+          CaptureFields[I->getCapturedVar()->getCanonicalDecl()] = *Field;
       }
     }
 
@@ -249,7 +249,7 @@ public:
 
     /// \brief Lookup the captured field decl for a variable.
     virtual const FieldDecl *lookup(const VarDecl *VD) const {
-      return CaptureFields.lookup(VD);
+      return CaptureFields.lookup(VD->getCanonicalDecl());
     }
 
     bool isCXXThisExprCaptured() const { return getThisFieldDecl() != nullptr; }
